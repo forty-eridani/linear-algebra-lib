90b8b804a94277191d41931ad65f5e716d2fcb54
nolanthw8s@gmail.com <97766806+forty-eridani@users.noreply.github.com>
2025-05-03 10:47:34 -0500

    Remove main function content

--- src/c/main.c
+++ src/c/main.c
@@ -1,14 +1,5 @@
 #include "matrix.h"
 
-#include <stdlib.h>
 
 int main() {
-	double matrixData[] = {
-		1.2, 1.3, 1.4,
-		1.6, 2.1, 4.3
-	};
-
-	Matrix m = CreateMatrixWithElements(NULL, 2, 3);
-
-	PrintMatrix(m);
 }
